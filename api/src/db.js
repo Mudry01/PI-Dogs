329976174db657aaeac8a6bc4256f4ff7c32726a
Sebastian Mudry <sebastiandiego_01@hotmail.com>
2023-10-11 18:08:06 -0300

    Modificaciones

--- api/src/db.js
+++ api/src/db.js
@@ -30,12 +30,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Dog, Temperament, Dog_Temperament } = sequelize.models;
+const { Dog, Temperament } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
-Dog.belongsToMany(Temperament, {through: 'dog_temperament'});
+Dog.belongsToMany(Temperament, {through: 'dog_temperament' });
 Temperament.belongsToMany(Dog, {through: 'dog_temperament'});
 
 module.exports = {
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const dogRouter = require('./DogRoutes');
+// const TemperamentRouter = require('./TemperamentRoutes');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use("/dogs", dogRouter);
+// router.use('/temperament', TemperamentRouter);
 
 module.exports = router;
