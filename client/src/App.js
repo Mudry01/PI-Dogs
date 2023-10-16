02369668115ddfb8a6a3668d3e0da67fa638f897
Sebastian Mudry <sebastiandiego_01@hotmail.com>
2023-10-16 17:43:39 -0300

    Rutas agregadas y provider agregdo

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+//LAS VIEWS
+import LandingPage from './Views/LandingPage/LandingPage';
+import HomePage from './Views/HomePage/HomePage';
+import NavBar from './Components/NavBar/NavBar';
+
 function App() {
+
+  const location = useLocation();
+
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
+      {location.pathname !== "/" ? <NavBar/> : null}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
