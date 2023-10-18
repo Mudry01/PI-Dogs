aef04dfe26f67388928820298f93c78f1264829b
Sebastian Mudry <sebastiandiego_01@hotmail.com>
2023-10-18 11:14:06 -0300

    Se creo el filtrado

--- client/src/Redux/Actions.js
+++ client/src/Redux/Actions.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
 export const GET_ALL_DOGS = "GET_ALL_DOGS";
+export const GET_DOGS_BY_ID = "GET_DOGS_BY_ID";
+export const FILTER_SHOW_ALL = "FILTER_SHOW_ALL";
+export const FILTER_FROM_BDD = "FILTER_FROM_BDD";
+export const FILTER_FROM_API = "FILTER_FROM_API";
+export const FILTER_TEMP = "FILTER_TEMP";
 
 export const getAllDogs = () => {
-    return async function (dispatch){
+    return async function (dispatch) {
         try {
             const endpoint = "http://localhost:3001/dogs";
             const response = await axios.get(endpoint);
@@ -12,9 +17,59 @@ export const getAllDogs = () => {
                 type: GET_ALL_DOGS,
                 payload: response.data,
             });
-            
+
         } catch (err) {
             console.log(err);
         }
     }
-}
+};
+
+export const getDogsById = (id) => {
+    return async function (dispatch) {
+        try {
+            const endpoint = `http://localhost:3001/dogs/${id}`;
+            const response = await axios.get(endpoint);
+
+            dispatch({
+                type: GET_DOGS_BY_ID,
+                payload: response.data,
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+};
+
+export const filterTemp = () => {
+    return async function (dispatch) {
+        try{
+            const endpoint = "http://localhost:3001/temperament";
+            const response = await axios.get(endpoint);
+
+            dispatch({
+                type: FILTER_TEMP,
+                payload: response.data,
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+};
+
+export const filterFromBdd = () => {
+    return {
+        type: FILTER_FROM_BDD,
+    };
+};
+
+export const filterFromApi = () => {
+    return {
+        type: FILTER_FROM_API,
+    };
+};
+
+export const filterShowAll = () => {
+    return {
+        type: FILTER_SHOW_ALL,
+    };
+};
--- client/src/Redux/Reducer.js
+++ client/src/Redux/Reducer.js
@@ -1,9 +1,17 @@
 import {
     GET_ALL_DOGS,
+    GET_DOGS_BY_ID,
+    FILTER_SHOW_ALL,
+    FILTER_FROM_BDD,
+    FILTER_FROM_API,
+    FILTER_TEMP
 } from "./Actions";
 
 const initialState = {
     dogs: [],
+    allDogs: [],
+    dogsDetail: [],
+    temperaments: [],
 };
 
 const rootReducer = (state = initialState, action) => {
@@ -12,7 +20,38 @@ const rootReducer = (state = initialState, action) => {
         case GET_ALL_DOGS:
             return {
                 ...state,
-                dogs: action.payload
+                dogs: action.payload,
+                allDogs: action.payload,
+            };
+
+        case GET_DOGS_BY_ID:
+            return {
+                ...state,
+                dogsDetail: action.payload,
+            };
+
+        case FILTER_FROM_BDD:
+            return {
+                ...state,
+                dogs: state.allDogs.filter((dog) => dog.createdInBd === true),
+            };
+
+        case FILTER_FROM_API:
+            return {
+                ...state,
+                dogs: state.allDogs.filter((dog) => dog.createdInBd === false),
+            };
+
+        case FILTER_SHOW_ALL:
+            return {
+                ...state,
+                dogs: state.allDogs,
+            };
+
+        case FILTER_TEMP:
+            return {
+                ...state,
+                temperaments: action.payload,
             };
         default:
             return { ...state };
